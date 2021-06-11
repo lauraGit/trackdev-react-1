7ed30c75f09104f6c9d4131eb23e1a4299e8b4fd
Laura <3987084+lauraGit@users.noreply.github.com>
2021-06-11 20:22:21 +0200

    Add CreateCourse component

--- src/components/CreateCourse.js
+++ src/components/CreateCourse.js
@@ -1,7 +1,74 @@
-const CreateCourse = () => {
-  return (
-    <p>Here you will create a course</p>
-  )
+import { Component } from 'react'
+
+class CreateCourse extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      created: false,
+      hasApiError: false,
+      errorMessage: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({ hasApiError: false, errorMessage: ''})
+    this.requestCreate()
+  }
+
+  requestCreate() {
+    var requestBody = {
+      name: this.state.name
+    }
+    fetch('https://localhost:8080/courses', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: 'include',
+        body: JSON.stringify(requestBody)
+    })
+      .then(async response => {
+        if(!response.ok) {
+          const errorData = await response.json();
+          this.setState({ hasApiError: true, errorMessage: errorData.message || 'Unknown error from server'})
+        } else {
+          this.setState({ created: true })
+        }
+      })
+      .catch(error => {
+        this.setState({ hasApiError: true, errorMessage: 'Unexpected error'})
+      })
+  }
+
+  handleInputChange(e) {
+    let inputChange = {}
+    inputChange[e.target.name] = e.target.value
+    this.setState(inputChange)
+  }
+
+  render() {
+    if(this.state.created) {
+      return (<div><p>You have created the new course successfully.</p></div>)
+    }
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input name="name" value={this.state.name} onChange={this.handleInputChange}/>
+          </label>          
+          <button type="submit">Create</button>
+          {
+            this.state.hasApiError ? (<p>{this.state.errorMessage}</p>) : null
+          }
+        </form>
+      </div>
+    )
+  }
 }
 
 export default CreateCourse
