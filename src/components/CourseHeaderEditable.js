9a8acabd9afb39f7d4d477fd17ff0c027426c7d7
Laura <3987084+lauraGit@users.noreply.github.com>
2021-07-02 10:38:36 +0200

    Ask for confirmation when deleting a Course

--- src/components/CourseHeaderEditable.js
+++ src/components/CourseHeaderEditable.js
@@ -1,6 +1,7 @@
 import { Component } from "react"
 import { Redirect } from "react-router-dom"
 import Api from '../utils/api'
+import ConfirmationModal from './ConfirmationModal'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/Col'
@@ -21,6 +22,8 @@ class CourseHeaderEditable extends Component {
     this.handleInputChange = this.handleInputChange.bind(this)
     this.handleDeleteClick = this.handleDeleteClick.bind(this)
     this.handleDismissAlert = this.handleDismissAlert.bind(this)
+    this.handleCancelDelete = this.handleCancelDelete.bind(this)
+    this.handleActualDelete = this.handleActualDelete.bind(this)
   }
 
   handleEditClick() {
@@ -60,14 +63,22 @@ class CourseHeaderEditable extends Component {
     this.setState(inputChange)
   }
 
-  handleDeleteClick() {
+  handleActualDelete() {
     Api.delete('/courses/' + this.props.course.id)
       .then(data => {
-          this.setState({mode: "deleted", errors: {}})
+        this.setState({mode: "deleted", errors: {}, askConfirmDelete: false})
       })
       .catch(error => {
-        this.setState({ errors: { delete: error?.details?.message || 'Unknown error' }})
+        this.setState({ errors: { delete: error?.details?.message || 'Unknown error' }, askConfirmDelete: false })
       })
+  }
+
+  handleCancelDelete() {
+    this.setState({askConfirmDelete: false})
+  }
+
+  handleDeleteClick() {
+    this.setState({askConfirmDelete: true})
   }
 
   handleDismissAlert() {
@@ -127,6 +138,14 @@ class CourseHeaderEditable extends Component {
             </Button>
           </Col>
         </Form.Row>
+        <ConfirmationModal show={this.state.askConfirmDelete}
+            title="Sure you want to delete this course?"
+            onCancel={this.handleCancelDelete}
+            onConfirm={this.handleActualDelete}
+            >
+          <p>Please keep in mind that when a course is deleted all its groups, invites, backlogs and tasks are deleted as well.</p>
+          <p>You cannot recover from this action.</p>
+        </ConfirmationModal>
         <div>
           {
             this.state.errors.delete
