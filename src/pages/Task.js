ef73fc3a38b3cdbe6484964860428e9a9cf26937
Laura <3987084+lauraGit@users.noreply.github.com>
2021-07-17 17:32:16 +0200

    Edit Estimation, Status and Assigne inline (with confirmation)

--- src/pages/Task.js
+++ src/pages/Task.js
@@ -7,12 +7,23 @@ import TaskStatus from '../components/TaskStatus'
 import withData from "../components/withData"
 import Api from '../utils/api'
 import EstimationPoints from "../components/EstimationPoints"
+import EditableField from '../components/EditableField'
 import EditableHeader from "../components/EditableHeader"
 
 const Task = ( { task, onDataTouched }) => {
   const [ errors, setErrors ] = useState({})
+
+  // name
   const [ isEditing, setIsEditing ] = useState(false)
 
+  const [ estimationEditStatus, setEstimationEditStatus ] = useState({ status: 'normal', error: null})
+  const [ statusEditStatus, setStatusEditStatus ] = useState({ status: 'normal', error: null})
+  const [ assigneeEditStatus, setAssigneeEditStatus ] = useState({ status: 'normal', error: null})
+
+  const statusOptions = [ "TODO", "INPROGRESS", "DONE", "TESTED", "SPRINT", "DELETED", "CREATED" ]
+
+  // NAME EDIT
+  // --------------------------------------------------------------
   function handleNameChange(newName) {
     var requestBody = {
       name: newName
@@ -27,6 +38,59 @@ const Task = ( { task, onDataTouched }) => {
       })
   }
 
+  // ESTIMATION EDIT
+  // --------------------------------------------------------------
+  function handleEstimationChange(newEstimation) {
+    var requestBody = {
+      estimationPoints: newEstimation === '' ? null : newEstimation
+    }
+    setEstimationEditStatus({ status: 'saving' })
+    Api.patch(`/tasks/${task.id}`, requestBody)
+      .then(data => {     
+        onDataTouched()
+        setEstimationEditStatus({ status: 'saved' })
+      })
+      .catch(error => {
+        console.log('setting new status to error')
+        setEstimationEditStatus({ status: 'error', error: error?.details?.message || 'Unknown error' })
+      })
+  }
+
+  // STATUS EDIT
+  // --------------------------------------------------------------
+  function handleStatusChange(newStatus) {
+    var requestBody = {
+      status: newStatus
+    }
+    setStatusEditStatus({ status: 'saving' })
+    Api.patch(`/tasks/${task.id}`, requestBody)
+      .then(data => {     
+        onDataTouched()
+        setStatusEditStatus({ status: 'saved' })
+      })
+      .catch(error => {
+        setStatusEditStatus({ status: 'error', error: error?.details?.message || 'Unknown error' })
+      })
+  }
+
+  // ASSIGNEE EDIT
+  // --------------------------------------------------------------
+  function handleAssigneeChange(newAssignee) {
+    var requestBody = {
+      assignee: newAssignee === '' ? null : newAssignee
+    }
+    setAssigneeEditStatus({ status: 'saving' })
+    Api.patch(`/tasks/${task.id}`, requestBody)
+      .then(data => {     
+        onDataTouched()
+        setAssigneeEditStatus({ status: 'saved' })
+      })
+      .catch(error => {
+        setAssigneeEditStatus({ status: 'error', error: error?.details?.message || 'Unknown error' })
+      })
+  }
+
+  // RENDER
   if(!task) {
     return null
   }
@@ -47,16 +111,40 @@ const Task = ( { task, onDataTouched }) => {
                 />
         </div>        
       </div>
-      
-      <p>Reporter: <UserMention user={task.reporter} /></p>
-      <p>Created at: {new Date(task.createdAt).toLocaleDateString()}</p>
-      <p>
-        Assignee: { task.assignee ? <UserMention user={task.assignee} /> : '-' }
-      </p>
-      <p>
-        Estimation: { task.estimationPoints ? <EstimationPoints estimationPoints={task.estimationPoints} /> : '-'}
-      </p>
-      <p>Status: <TaskStatus status={task.status}/></p>
+      <div>
+        <div>Reporter: <UserMention user={task.reporter} /></div>
+        <div>Created at: {new Date(task.createdAt).toLocaleDateString()}</div>
+        <div>
+          Assignee: <EditableField
+            fieldView={ task.assignee ? <UserMention user={task.assignee} /> : '-' }
+            status={assigneeEditStatus.status}
+            error={assigneeEditStatus.error}
+            value={task.assignee?.username}
+            onChange={handleAssigneeChange} />
+        </div>
+        <div>
+          Estimation: <EditableField
+            fieldView={(task.estimationPoints ? <EstimationPoints estimationPoints={task.estimationPoints} /> : '-')}
+            status={estimationEditStatus.status}
+            error={estimationEditStatus.error}
+            value={task.estimationPoints}
+            onChange={handleEstimationChange} />
+        </div>
+        <div>
+          Status: <EditableField
+            fieldView={(<TaskStatus status={task.status}/>)}
+            as="select"
+            status={statusEditStatus.status}
+            error={statusEditStatus.error}
+            value={task.status}
+            options={statusOptions}
+            onChange={handleStatusChange}>
+              {
+                  statusOptions.map(option => (<option key={option} value={option}>{option}</option>))
+              }
+            </EditableField>
+        </div>
+      </div>      
     </div>
   ) 
 }
